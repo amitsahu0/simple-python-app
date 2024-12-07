d77e9e119b286a84758efb64100bcab9faab5f73
Amit Sahu <129931373+amitsahu0@users.noreply.github.com>
2024-12-08 00:02:49 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -e
 
-#connect to docker hub
+# Log in to Amazon ECR
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 474668411047.dkr.ecr.ap-south-1.amazonaws.com
 
+# Pull the latest Docker image
+docker pull 474668411047.dkr.ecr.ap-south-1.amazonaws.com/simple-python-app:latest
 
-# Pull the Docker image from Docker Hub
-docker pull 474668411047.dkr.ecr.ap-south-1.amazonaws.com/simple-python-app
+# Stop any running container with the same name
+docker stop simple-python-app || true
+docker rm simple-python-app || true
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 474668411047.dkr.ecr.ap-south-1.amazonaws.com/simple-python-app
+# Run the Docker container
+docker run -d --name simple-python-app -p 5000:5000 474668411047.dkr.ecr.ap-south-1.amazonaws.com/simple-python-app:latest
