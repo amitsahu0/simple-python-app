7c9e6c5f6b2288ac26697856e7c6086399323aaa
Amit Sahu <129931373+amitsahu0@users.noreply.github.com>
2024-12-08 00:01:13 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+#connect to docker hub
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 474668411047.dkr.ecr.ap-south-1.amazonaws.com
+
+
 # Pull the Docker image from Docker Hub
 docker pull 474668411047.dkr.ecr.ap-south-1.amazonaws.com/simple-python-app
 
